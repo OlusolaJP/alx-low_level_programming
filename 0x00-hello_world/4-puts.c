0f26c2e6ba62726a970f704cccfda6f979b3a2fa
Olusola <adeotioluwasola1@gmail.com>
2022-09-09 07:19:28 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
-#include <studio.h>
+/*
+ * File: 4-puts.c
+ * Auth: Brennam D Baraban <375@horbertonschool.com>
+ */
+
+#include<stdio>
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints "Programming is buiding a multilingual
+ * 		 puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
