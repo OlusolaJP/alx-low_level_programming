05b290bfe6833aaeba23c2e508d93bb24ba0759e
Olusola <adeotioluwasola1@gmail.com>
2022-09-10 00:36:38 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,12 @@
 /**
 * main - print the string in the put function
 *
-* this program prints "Programming is like building a multilingual puzzle
+*Description: using the main function
+*this program prints "Programming is like building a multilingual puzzle
 *Return: 0
 */
 int main(void)
 {
-	printf{"with proper grammar, but the outcome is a piece of art, \n");
+	printf(*with proper grammar, but the outcome is a piece of art,\n*);
 	return (0);
 }
