7be6c2c089a6a040530720eeb5f7e2497e58f650
Olusola <adeotioluwasola1@gmail.com>
2022-09-11 14:00:35 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ float afloat;
 	printf("Size of a char: %zu byte(s)\n", sizeof(acharacter));
 	printf("Size of an int: %zu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(along));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(alongalong));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %zu byte(s)\n", sizeof(afloat));
 	return (0);
 }
